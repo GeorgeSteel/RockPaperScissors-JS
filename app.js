8c608f169181c3af31500b09110e1d3d2129e313
jorge <julianacero98@gmail.com>
2019-01-02 23:30:43 -0600

    JS started

---
+++ app.js
@@ -0,0 +1,11 @@
+const userScore = 0;
+const computerScore = 0;
+const userScoreSpan = document.querySelector('#user-score');
+const computerScoreSpan = document.querySelector('#computer-score');
+const scoreBoardDiv = document.querySelector('.score-board');
+const resultDiv = document.querySelector('.result');
+const rockDiv = document.querySelector('#r');
+const paperDiv = document.querySelector('#p');
+const scissorsDiv = document.querySelector('#s');
+
+
